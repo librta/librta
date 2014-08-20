12cd0b136a3f37bf738f6c15aa45f629db3dd78c
Frederic Roussel <fr.frasc@gmail.com>
2014-08-20 16:50:17 -0700

    actual version 1.0.0 that somehow was never commited.

---
+++ src/parse.h
@@ -0,0 +1,24 @@
+#ifndef YYSTYPE
+#define YYSTYPE int
+#endif
+#define	SELECT	257
+#define	UPDATE	258
+#define	FROM	259
+#define	WHERE	260
+#define	NAME	261
+#define	STRING	262
+#define	INTEGER	263
+#define	REALNUM	264
+#define	LIMIT	265
+#define	OFFSET	266
+#define	SET	267
+#define	EQ	268
+#define	NE	269
+#define	GT	270
+#define	LT	271
+#define	GE	272
+#define	LE	273
+#define	AND	274
+
+
+extern YYSTYPE yylval;
