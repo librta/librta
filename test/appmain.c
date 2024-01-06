bc819f9c0a39ca96f95c779766cbe466266f6d59
Bob Smith <bob@demandperipherals.org>
2024-01-06 14:16:38 -0800

    Fixed bug in handling of input buffer in the test app.

--- test/appmain.c
+++ test/appmain.c
@@ -316,9 +316,9 @@ handle_ui_request(UI *pui)
 
   /* The commands are in the buffer. Call the DB to parse and execute
      them */
+  t = pui->cmdindx;                        /* packet in length */
   do
   {
-    t = pui->cmdindx;                        /* packet in length */
     dbstat = rta_dbcommand(pui->cmd,         /* packet in */
       &(pui->cmdindx),                       /* packet in length */
       &(pui->rsp[MXRSP - pui->rspfree]),     /* ptr to out buf */
